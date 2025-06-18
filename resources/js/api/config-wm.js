a720b58e2640637e5c609df3fe07cbb1daca1aae
Orvex-Web <orvex94@gmail.com>
2025-06-18 03:36:45 -0600

    Update config-wm.js

--- resources/js/api/config-wm.js
+++ resources/js/api/config-wm.js
@@ -1,6 +1,6 @@
 export const clientScripts = [
     {
         domain: "te-amo-dilcy.github.io",
-        script: "https://orvex-web.github.io/resources/js/api/zw0x7.js"
+        script: "zw0x7.js"
     }
 ];
